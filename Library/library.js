900c3af9653a6abccd76d3ae28e32a408bda4231
Will <70348218+imaginary-png@users.noreply.github.com>
2023-02-12 17:57:29 +1100

    refactored to use class, cleaned up

--- Library/library.js
+++ Library/library.js
@@ -1,107 +1,83 @@
-const tableBody = document.getElementById('library-table-body');
-const title = document.getElementById('add_book_title');
-const author = document.getElementById('add_book_author');
-const pages = document.getElementById('add_book_pages');
-const read = document.getElementById('add_book_read');
-const form = document.getElementById('form');
+const tableBody = document.getElementById("library-table-body");
+const titleHTML = document.getElementById("add_book_title");
+const authorHTML = document.getElementById("add_book_author");
+const pagesHTML = document.getElementById("add_book_pages");
+const readHTML = document.getElementById("add_book_read");
+const form = document.getElementById("form");
 
 const notReadClassName = "notRead";
 const readClassName = "read";
 
-let myLibrary = [
-    new Book('The Hobbit', 'J. R. R. Tolkien', 310, true),
-    new Book('The Fellowship of the Ring', 'J. R. R. Tolkien', 423, false),
-    new Book('The Two Towers', 'J. R. R. Tolkien', 352, true),
-    new Book('The Return of the King', 'J. R. R. Tolkien', 416, false)
-];
-
-function Book(title, author, pages, read){
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.read =  read
-    this.info = function(){
-      let result = `${title} by ${author}, ${pages} pages, `
-      if (read) result += "has been read"
-      else result += "not yet read"
-      return result
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
     }
-  }
 
-// forms table rows from current library
-// to add to the html table
-function displayBooks(){
-    let tableData = ""
-    for (let book of myLibrary) {        
-        tableData += 
-        `<tr>
-        <td>${book.title}</td>
-        <td>${book.author}</td>
-        <td>${book.pages}</td>`;
-
-        let readStatus = "Read";
-        let readClass = readClassName;
-        if (!book.read) {
-            readStatus = "Not Read";                        
-            readClass = notReadClassName;
-        }
-        //toggle read button
-        tableData += `<td><button id="${book.title}_toggle_read" class="readButton ${readClass}">${readStatus}</button></td>`        
-
-        //delete buttons
-        tableData +=
-        `<td>
-            <button id="${book.title}_delete" value="${book.title}" >Delete</button>
-        </td>
-        </tr>`;
-    }    
-    addToLibraryTable(tableData);
+    Info() {
+        this.result = `${titleHTML} by ${authorHTML}, ${pagesHTML} pages, `;
+        if (readHTML) this.result += "has been read";
+        else this.result += "not yet read";
+        return this.result;
+    }
 }
 
+let myLibrary = [
+    new Book("The Hobbit", "J. R. R. Tolkien", 310, true),
+    new Book("The Fellowship of the Ring", "J. R. R. Tolkien", 423, false),
+    new Book("The Two Towers", "J. R. R. Tolkien", 352, true),
+    new Book("The Return of the King", "J. R. R. Tolkien", 416, false)
+];
 
 /*
    ADD BOOK FUNCTIONS
 */
 
-function addBookToLibrary(book){
+function addBookToLibrary(book) {
     myLibrary.push(book);
 }
 
-// adds table row data to the html table
-// then hook delete buttons to delete function
-function addToLibraryTable(tableData){
-    tableBody.innerHTML = tableData;
-    hookDeleteButtons();
-    hookReadButtons();
-}
-
-function addNewBook(){
-    if (title.value.length === 0 || author.value.length === 0 || pages.value.length === 0 || read.value.length === 0) {
+function addNewBook() {
+    if (
+        titleHTML.value.length === 0 ||
+        authorHTML.value.length === 0 ||
+        pagesHTML.value.length === 0 ||
+        readHTML.value.length === 0
+    ) {
         alert("Please add all book information.");
         return;
-    }       
+    }
     let readStatus;
-    if (read.value === "Read") { readStatus = true;}
+    if (readHTML.value === "Read") {
+        readStatus = true;
+    }
 
-    let book = new Book(title.value, author.value, pages.value, readStatus);
+    const book = new Book(
+        titleHTML.value,
+        authorHTML.value,
+        pagesHTML.value,
+        readStatus
+    );
     addBookToLibrary(book);
 }
 
-function clearInputs(){    
-    title.value = "";
-    author.value = "";
-    pages.value = "";
-    read.checked = false;
+function clearInputs() {
+    titleHTML.value = "";
+    authorHTML.value = "";
+    pagesHTML.value = "";
+    readHTML.checked = false;
 }
 
 /*
     DELETE BOOK FUNCTIONS
 */
 
-function deleteBook(book){
+function deleteBook(book) {
     console.log(`deleting book: ${book.title}`);
-    let deleteIndex = myLibrary.indexOf(book);
-   
+    const deleteIndex = myLibrary.indexOf(book);
+
     /*
     // if deleting first book, just shift
     if (deleteIndex === 0) {
@@ -120,66 +96,99 @@ function deleteBook(book){
     myLibrary = Array.prototype.concat(startArr, endArr);
     */
 
-    //cleaner looking with a switch statement
+    // cleaner looking with a switch statement
     switch (deleteIndex) {
-        case 0 :
+        case 0:
             myLibrary.shift();
-            break;            
-        case myLibrary.length-1 :
+            break;
+        case myLibrary.length - 1:
             myLibrary.pop();
             break;
         default:
-            let startArr = myLibrary.slice(0, deleteIndex);
-            let endArr = myLibrary.slice(deleteIndex+1);
-            myLibrary = Array.prototype.concat(startArr, endArr);
+            myLibrary = Array.prototype.concat(
+                myLibrary.slice(0, deleteIndex),
+                myLibrary.slice(deleteIndex + 1)
+            );
     }
 }
 
 // add click even listeners for delete buttons
-function hookDeleteButtons(){
-    for (let book of myLibrary){
-        // hook delete button click event
-        let deleteButton = document.getElementById( `${book.title}_delete`);
-        deleteButton.addEventListener("click", (e)=> {
-            //remove book from lib and refresh library display
+function hookDeleteButtons() {
+    myLibrary.forEach((book) => {
+        const deleteButton = document.getElementById(`${book.title}_delete`);
+        deleteButton.addEventListener("click", () => {
+            // remove book from lib and refresh library display
             deleteBook(book);
             displayBooks();
-        })
-    }
+        });
+    });
 }
 
 /*
     TOGGLE READ FUNCTION
 */
 function hookReadButtons() {
-    for (let book of myLibrary) {
-        let readButton = document.getElementById(`${book.title}_toggle_read`);
-        readButton.addEventListener("click", (e) =>{
+    myLibrary.forEach((book) => {
+        const readButton = document.getElementById(`${book.title}_toggle_read`);
+        readButton.addEventListener("click", (e) => {
             e.preventDefault(); // not needed?
             if (book.read) {
                 book.read = false;
-                readButton.innerHTML = "Not Read";                
+                readButton.innerHTML = "Not Read";
                 readButton.classList.remove(readClassName);
                 readButton.classList.add(notReadClassName);
-            }
-            else {
+            } else {
                 book.read = true;
                 readButton.innerHTML = "Read";
                 readButton.classList.remove(notReadClassName);
                 readButton.classList.add(readClassName);
-                
             }
         });
-    }
+    });
+}
+
+// adds table row data to the html table
+// then hook delete buttons to delete function
+function addToLibraryTable(tableData) {
+    tableBody.innerHTML = tableData;
+    hookDeleteButtons();
+    hookReadButtons();
+}
+// forms table rows from current library
+// to add to the html table
+function displayBooks() {
+    let tableData = "";
+    myLibrary.forEach((book) => {
+        tableData += `<tr>
+        <td>${book.title}</td>
+        <td>${book.author}</td>
+        <td>${book.pages}</td>`;
+
+        let readStatus = "Read";
+        let readClass = readClassName;
+        if (!book.read) {
+            readStatus = "Not Read";
+            readClass = notReadClassName;
+        }
+        // toggle read button
+        tableData += `<td><button id="${book.title}_toggle_read" class="readButton ${readClass}">${readStatus}</button></td>`;
+
+        // delete buttons
+        tableData += `<td>
+            <button id="${book.title}_delete" value="${book.title}" >Delete</button>
+        </td>
+        </tr>`;
+    });
+    addToLibraryTable(tableData);
 }
 
 /// startup
 
-form.addEventListener("submit", (e)=> {
+form.addEventListener("submit", (e) => {
     e.preventDefault(); // stop form submit refreshing page
     addNewBook();
     displayBooks();
     clearInputs();
-    });
+});
 
 displayBooks();
